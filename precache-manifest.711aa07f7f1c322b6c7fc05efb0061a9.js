3152ad684f85407d3b24830775e0f9f1912817c6
Sergi Alcaraz <sergi_alca@hotmail.com>
2020-10-17 17:00:42 +0200

    Updates

---
+++ precache-manifest.711aa07f7f1c322b6c7fc05efb0061a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fb510f054cfea5bb36cb22c7c00ac1b",
+    "url": "/phoBlock/index.html"
+  },
+  {
+    "revision": "577c572a80de019b9321",
+    "url": "/phoBlock/static/css/main.fd80e6f9.chunk.css"
+  },
+  {
+    "revision": "9d5666f5643a0c804354",
+    "url": "/phoBlock/static/js/2.80104d79.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/phoBlock/static/js/2.80104d79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "577c572a80de019b9321",
+    "url": "/phoBlock/static/js/main.98a25a4e.chunk.js"
+  },
+  {
+    "revision": "b389370c8965065aae0f",
+    "url": "/phoBlock/static/js/runtime-main.c8d668a1.js"
+  }
+]);
