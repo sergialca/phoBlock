d44c27ddea5fe51632d1670e5e44bd9980f3c2de
Sergi Alcaraz <sergi_alca@hotmail.com>
2020-11-14 13:57:21 +0100

    Updates

---
+++ precache-manifest.688a3d7d752458f9702f23b796e9040e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c12d72d83120f6fe15300befd301538f",
+    "url": "/phoblock/index.html"
+  },
+  {
+    "revision": "371d2b867b166d5a42cc",
+    "url": "/phoblock/static/css/main.c765a6d3.chunk.css"
+  },
+  {
+    "revision": "0990490249ae59aca5d5",
+    "url": "/phoblock/static/js/2.3df37f9d.chunk.js"
+  },
+  {
+    "revision": "fc489f12f3e474b5dcfb7b62296e3529",
+    "url": "/phoblock/static/js/2.3df37f9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "371d2b867b166d5a42cc",
+    "url": "/phoblock/static/js/main.d82b969a.chunk.js"
+  },
+  {
+    "revision": "c5a047a47ae5cde3ffad",
+    "url": "/phoblock/static/js/runtime-main.c218aa48.js"
+  },
+  {
+    "revision": "79b6ad78bfec42a09cca5825d6c38bb3",
+    "url": "/phoblock/static/media/header.79b6ad78.png"
+  },
+  {
+    "revision": "685cb46199b76a8f1bb62976e87d526b",
+    "url": "/phoblock/static/media/pho_logo.685cb461.png"
+  },
+  {
+    "revision": "ef3838430d61e8cf0e902ceac517feb4",
+    "url": "/phoblock/static/media/sign.ef383843.png"
+  }
+]);
